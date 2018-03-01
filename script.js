af57de3ba1cd09772293f36da79fbe9966422113
Sathes Balasingam <sathes352@gmail.com>
2018-03-01 17:45:06 -0500

    Finished

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $(".realColumns").on("click", function() {
     $("#all").css({
-      "display" : "grid",
       "grid-template-columns" : "repeat(" + this.id.length + ", 1fr)"
       })
   })
@@ -10,8 +9,34 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".realRows").on("click", function() {
     $("#all").css({
-      "display" : "grid",
-      "grid-auto-rows" : this.id.length*25
+      "grid-auto-rows" : this.id.length*100
+      })
+  })
+})
+
+$(document).ready(function() {
+  $(".rowz").on("click", function() {
+    $("#all").css({
+      "grid-row-gap" : "20px"
+      })
+  })
+})
+
+$(document).ready(function() {
+  $(".columnz").on("click", function() {
+    $("#all").css({
+      "grid-column-gap" : "20px"
+      })
+  })
+})
+
+$(document).ready(function() {
+  $(".reset").on("click", function() {
+    $("#all").css({
+      "grid-template-columns" : "repeat(3, 1fr)",
+      "grid-auto-rows" : "100px",
+      "grid-row-gap" : "0px",
+      "grid-column-gap" : "0px"
       })
   })
 })
