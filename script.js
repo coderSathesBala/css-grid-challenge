e0354c7cd03b172a8b5e093ceb0c92b3fa0d6e27
Sathes Balasingam <sathes352@gmail.com>
2018-03-01 14:56:06 -0500

    rowsAndColumnDone

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$('.sick').on('click', function() {
-  console.log('ok')
+$(document).ready(function() {
+  $(".realColumns").on("click", function() {
+    $("#all").css({
+      "display" : "grid",
+      "grid-template-columns" : "repeat(" + this.id.length + ", 1fr)"
+      })
+  })
+})
+
+$(document).ready(function() {
+  $(".realRows").on("click", function() {
+    $("#all").css({
+      "display" : "grid",
+      "grid-auto-rows" : this.id.length*25
+      })
+  })
 })
